8817ac0014baffdd8482c8c0f3a969e8e833c9bf
Boris <borismjimenezj@gmail.com>
2023-08-13 21:15:15 -0400

    se completo el proyecto para pc, falta responsiv

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+
+function validarFormulario() {
+    const nombre = document.getElementById("nombre").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const telefono = document.getElementById("telefono").value.trim();
+    const mensaje = document.getElementById("mensaje").value.trim();
+
+    if (nombre === "" || email === "" || telefono === "" || mensaje === "") {
+        console.log("Por favor, completa todos los campos.");
+        return false; // Evita que el formulario se envíe
+    }
+
+    console.log("Mensaje enviado");
+    return true; // El formulario se enviará si todo está bien
+
+}
